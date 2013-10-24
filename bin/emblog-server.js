9031de6c828a0448a18ca30e091baa30a8bc9db7
Guilherme Vieira (n2liquid) <super.driver.512@gmail.com>
2013-10-24 02:01:13 -0200

    Create a static file server with Express.

---
+++ bin/emblog-server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+app.use(express.logger());
+app.use(express['static'](__dirname + '/../public'));
+
+http.createServer(app).listen(3000);
+
