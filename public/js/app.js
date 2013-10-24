083cb86ec14ac8c4ba329673a08fbf367e3f0bb8
Guilherme Vieira (n2liquid) <super.driver.512@gmail.com>
2013-10-24 02:44:02 -0200

    Stub login functionality.

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,14 @@
 App = Ember.Application.create();
 
+App.ApplicationController = Ember.Controller.extend({
+	user: null,
+
+	actions: {
+		login: function () {
+			this.set('user', { name: this.login });
+			this.set('login', '');
+			this.set('password', '');
+		}
+	}
+});
+
