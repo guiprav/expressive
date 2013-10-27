74eb26d5063378dd096ebf9bae1dc6b012e1ea26
Guilherme Vieira (n2liquid) <super.driver.512@gmail.com>
2013-10-27 20:52:56 -0200

    Create compose routes, markup, and styling.

---
+++ src/post.js
@@ -0,0 +1,3 @@
+module.exports = function (title, body, tags, cb) {
+	cb(null);
+};
---
+++ src/request-handlers/compose.js
@@ -0,0 +1,57 @@
+var post = require('../post');
+var templates = require('../templates');
+
+module.exports = function (app) {
+	app.get('/compose', function (req, res) {
+		res.send(templates.page({
+			user: req.session.user,
+			body: templates.compose({
+				authenticated: !!req.session.user
+			})
+		}));
+	});
+
+	app.post('/compose', function (req, res) {
+		var tags = req.body.tags.replace(/, +/g, ',');
+		tags = tags.split(',');
+
+		post(req.body.title, req.body.body, tags, function (err) {
+			if (err) {
+				if (!err.user_presentable) {
+					res.send(500, templates.page({
+						user: req.session.user,
+						body: templates.compose({
+							error: 'Internal server error.',
+							authenticated: !!req.session.user,
+							title: req.body.title,
+							body: req.body.body,
+							tags: req.body.tags
+						})
+					}));
+				}
+				else {
+					res.send(templates.page({
+						user: req.session.user,
+						body: templates.compose({
+							error: err.message,
+							authenticated: !!req.session.user,
+							title: req.body.title,
+							body: req.body.body,
+							tags: req.body.tags
+						})
+					}));
+				}
+
+				return;
+			}
+
+			res.send(templates.page({
+				user: req.session.user,
+				body: templates.compose({
+					success: 'Post published successfuly!',
+					authenticated: !!req.session.user
+				})
+			}));
+		});
+	});
+};
