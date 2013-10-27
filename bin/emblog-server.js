15d75555493ddad100816332a513763cc5aae0a7
Guilherme Vieira (n2liquid) <super.driver.512@gmail.com>
2013-10-27 18:58:26 -0200

    Implement account creation logic.

--- bin/emblog-server.js
+++ bin/emblog-server.js
@@ -47,6 +47,59 @@ app.get('/create-account', function (req, res) {
 	}));
 });
 
+app.post('/create-account', function (req, res) {
+	var user = (req.session)? req.session.user : null;
+
+	if (req.body.password !== req.body['repeated-password']) {
+		res.send(templates.page({
+			user: user,
+
+			body: templates.create_account({
+				error: "The passwords you've entered don't match. Try again.",
+				name: req.body.name,
+				email: req.body.email
+			})
+		}));
+
+		return;
+	}
+
+	auth.create(req.body.email, req.body.password, req.body.name, function (err) {
+		if (err) {
+			if (!err.user_presentable) {
+				res.send(500, templates.page({
+					user: user,
+
+					body: templates.create_account({
+						error: 'Internal server error.'
+					})
+				}));
+			}
+			else {
+				res.send(templates.page({
+					user: user,
+
+					body: templates.create_account({
+						error: err.message,
+						name: req.body.name,
+						email: req.body.email
+					})
+				}));
+			}
+
+			return;
+		}
+
+		res.send(templates.page({
+			user: user,
+
+			body: templates.create_account({
+				success: 'Account created successfuly!'
+			})
+		}));
+	});
+});
+
 app.post('/login', function (req, res) {
 	auth(req.body.email, req.body.password, function (err, user_data) {
 		if (err) {
--- src/auth.js
+++ src/auth.js
@@ -20,3 +20,60 @@ module.exports = function (email, password, cb) {
 	});
 };
 
+module.exports.create = function (email, password, name, cb) {
+	if (name === '') {
+		var err = new Error('Name must not be empty.');
+		err.user_presentable = true;
+
+		cb(err);
+
+		return;
+	}
+
+	if (email === '') {
+		var err = new Error('Email must not be empty.');
+		err.user_presentable = true;
+
+		cb(err);
+
+		return;
+	}
+
+	if (password.length < 4) {
+		var err = new Error('Password must have at least 4 characters.');
+		err.user_presentable = true;
+
+		cb(err);
+
+		return;
+	}
+
+	var users = db.handle.collection('users');
+
+	users.findOne({ email: email }, function (err, existing_user) {
+		if (err) {
+			cb(err);
+			return;
+		}
+
+		if (existing_user) {
+			var err = new Error('A user with this email address already exists in the database.');
+			err.user_presentable = true;
+
+			cb(err);
+
+			return;
+		}
+		else {
+			users.insert({ name: name, email: email, password: password }, function (err) {
+				if (err) {
+					cb(err);
+					return;
+				}
+
+				cb(null);
+			});
+		}
+	});
+};
+
