3afb25199fe34ae64d44f997669f753469b934ec
Guilherme Vieira (n2liquid) <super.driver.512@gmail.com>
2013-10-28 00:32:55 -0200

    Add production Mongo URI.

--- src/database.js
+++ src/database.js
@@ -1,10 +1,17 @@
 var mongo = require('mongodb').MongoClient;
 
+if (process.env.NODE_ENV === 'production') {
+	var mongo_uri = 'mongodb://emblog:ashfgaosgnoekgnwoegksakgm@ds053148.mongolab.com:53148/heroku_app18867064';
+}
+else {
+	var mongo_uri = 'mongodb://localhost/emblog';
+}
+
 module.exports = {
 	handle: null,
 
 	connect: function (cb) {
-		mongo.connect('mongodb://localhost/emblog', function (err, db) {
+		mongo.connect(mongo_uri, function (err, db) {
 			if (err) {
 				cb(err);
 				return;
