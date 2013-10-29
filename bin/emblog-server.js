8a1bf4e8539131367328447497dbb1b5a26ada76
Guilherme Vieira (n2liquid) <super.driver.512@gmail.com>
2013-10-29 03:52:55 -0200

    refactor: No need to be sure req.session.user is exactly null...

--- bin/emblog-server.js
+++ bin/emblog-server.js
@@ -14,7 +14,7 @@ app.use(express['static'](__dirname + '/../public'));
 
 app.use(function (req, res, next) {
 	if (!req.session) {
-		req.session = { user: null };
+		req.session = {};
 	}
 
 	next();
