bdc09ae26af9a15cf424cecc1b4252e0b042619c
Guilherme Vieira (n2liquid) <super.driver.512@gmail.com>
2013-10-31 04:00:31 -0200

    Only show "Edit or delete this post" link when logged in.

--- src/request-handlers/index.js
+++ src/request-handlers/index.js
@@ -3,15 +3,21 @@ var post = require('../post');
 
 module.exports = function (app) {
 	app.get('/', function (req, res) {
+		var template_data = {
+			logged_in: !!req.session.user
+		};
+
 		post.get(function (err, posts) {
 			if (err) {
 				res.push_error_object(err);
-				res.send_page('posts');
+				res.send_page('posts', template_data);
 
 				return;
 			}
 
-			res.send_page('posts', { posts: posts });
+			template_data.posts = posts;
+
+			res.send_page('posts', template_data);
 		});
 	});
 };
