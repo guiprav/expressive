63671c850412f170836466c2c2a54b46de9918b5
Guilherme Vieira (n2liquid) <super.driver.512@gmail.com>
2013-11-01 03:10:55 -0200

    Implement search functionality.

--- src/post.js
+++ src/post.js
@@ -111,3 +111,36 @@ module.exports.getById = function (post_id, cb) {
 	);
 };
 
+module.exports.search = function (title, tags, cb) {
+	var posts = db.handle.collection('posts');
+
+	var title_terms = title.split(/[ -]/).filter(
+		function (term) {
+			return term !== '';
+		}
+	).map(
+		function (term) {
+			return new RegExp(term, 'gi');
+		}
+	);
+
+	var query = {};
+
+	if (title_terms.length !== 0) {
+		query.title = { $all: title_terms };
+	}
+
+	if (tags.length !== 0) {
+		query.tags = { $all: tags };
+	}
+
+	posts.find(query, function (err, posts) {
+		if (err) {
+			cb(err);
+			return;
+		}
+
+		posts.toArray(cb);
+	});
+};
+
