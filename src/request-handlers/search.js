d4912cc6e61bb5426d06dcc51944db469a0f4871
Guilherme Vieira (n2liquid) <super.driver.512@gmail.com>
2013-11-01 03:08:07 -0200

    refactor: Style .blogpost directly (not only inside .posts-container).

---
+++ src/request-handlers/search.js
@@ -0,0 +1,40 @@
+var post = require('../post');
+
+module.exports = function (app) {
+	app.get('/article/search', function (req, res) {
+		res.send_page('search');
+	});
+
+	app.post('/article/search', function (req, res) {
+		var template_data = {
+			title: req.body.title,
+			tags: req.body.tags
+		};
+
+		var title = req.body.title.trim();
+
+		var tags = req.body.tags.replace(/, +/g, ',');
+		tags = tags.split(',');
+
+		if (tags.length === 1 && tags[0] === '') {
+			tags = [];
+		}
+
+		post.search(title, tags, function (err, posts) {
+			if (err) {
+				res.push_error_object(err);
+				res.send_page('search', template_data);
+
+				return;
+			}
+
+			if (posts.length === 0) {
+				res.push_message('warning', 'No posts matching your criteria have been found. Try simplifying them.');
+			}
+
+			template_data.posts = posts;
+
+			res.send_page('search', template_data);
+		});
+	});
+};
