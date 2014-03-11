cb683299c0e797521c59be7ba2dbb42cd106cc22
Guilherme Vieira <super.driver.512@gmail.com>
2014-03-11 12:13:40 -0300

    refactor: Trim comma-separated HTML inside the helper.

--- templates/comma-separated.helper.js
+++ templates/comma-separated.helper.js
@@ -1,3 +1,10 @@
 module.exports = function(hbs, array, options) {
-	return array.map(options.fn).join(', ');
+	return array
+			.map(options.fn)
+			.map (
+				function(html) {
+					return html.trim();
+				}
+			)
+			.join(', ');
 };
