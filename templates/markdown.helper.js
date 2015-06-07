7c8207b940e2d670fd551816ef85cacfbeec3685
Guilherme Vieira <super.driver.512@gmail.com>
2015-06-07 01:09:17 -0300

    Allow Markdown Handlebars helper specify whether to sanitize code or not.

--- templates/markdown.helper.js
+++ templates/markdown.helper.js
@@ -1,11 +1,14 @@
 var marked = require('marked');
-module.exports = function(hbs, text) {
+module.exports = function(hbs, text, options) {
 	text = text || '';
+	if(options.hash.sanitize === undefined) {
+		options.hash.sanitize = true;
+	}
 	return new hbs.SafeString (
 		marked (
 			text, {
 				gfm: true
-				, sanitize: true
+				, sanitize: options.hash.sanitize
 				, breaks: true
 				, tables: true
 			}
